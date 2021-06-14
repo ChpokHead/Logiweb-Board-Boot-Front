8d29b8eb25f9081db3bbb85c6c8bb8b11dbbd1e9
danilihsanov <mildgeneral@gmail.com>
2021-06-14 16:46:32 +0300

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DriverListComponent } from "./driver-list/driver-list.component";
+import { OrderListComponent } from "./order-list/order-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'drivers', component: DriverListComponent },
+  { path: 'orders', component: OrderListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { DriverListComponent } from './driver-list/driver-list.component';
+import { DriverService } from "./service/driver.service";
+import { OrderListComponent } from './order-list/order-list.component';
+import {OrderService} from "./service/order.service";
+import { TruckListComponent } from './truck-list/truck-list.component';
+import {TruckService} from "./service/truck.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DriverListComponent,
+    OrderListComponent,
+    TruckListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DriverService, OrderService, TruckService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
